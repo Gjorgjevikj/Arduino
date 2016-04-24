4dff92647829f53a2a3ebf0b836bae965793a0a9
Gjorgjevikj <dejan.gjorgjevikj@gmail.com>
2016-04-24 02:53:14 +0200

    Added files via upload
    Initial release

---
+++ PBMonitor_example.ino
@@ -0,0 +1,153 @@
+/*
+  PBMonitor v 1.0 - Interrupt driven push button monitor with simple debouncing
+  Triggers "an event" (calling a call back function) on release of the button
+  Reports back the duration in miliseconds thebutton has peen held down after releasing 
+  so that long and short presses can be determined from the user function invoked by the button
+  
+  The example circuit:
+   * LEDs on pins 4 and 6 to ground (+ resistors)
+   * switch (normally open) from pin 3 to GND (internal pull-up configured)
+   * switch (normally open) from pin 2 to Vcc with a 10K pull-down resistor
+
+ created 22.04.2016
+ by Dejan 
+ */
+
+#include "id_PBmonitor.h"
+
+// definition of the pins the LEDs are connected to
+#define LED_R 6
+#define LED_G 4
+
+// definition of the pins the push buttons are connected to 
+// can be active low (using internal puluup resistor) or high (external pulldown resistor must be installed)
+#define PB1 3
+#define PB2 2
+
+// An example callback function to be called when button1 is pressed
+// Turns on and off one by one the two LEDs in sequence with 100ms delay
+void FlashLeds(unsigned long n) 
+{
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  int t=100;
+
+  Serial.print(" Button held down for: ");
+  Serial.print(n);
+  Serial.print("ms  - ");
+  Serial.print("Service 1 started.");
+  for(int i=0; i<3; i++)
+  {
+    digitalWrite(LED_G, HIGH); 
+    delay(t); Serial.print('.');
+    digitalWrite(LED_R, HIGH); 
+    delay(t); Serial.print('.');
+    digitalWrite(LED_G, LOW); 
+    delay(t); Serial.print('.');
+    digitalWrite(LED_R, LOW); 
+    delay(3*t); Serial.print('.');
+  }  
+  Serial.println(" service 1 ended.");
+}
+
+// An example callback function to be called when button2 is pressed
+// Turns on and off both of the LEDs with 300ms delay
+void BlinkLeds(unsigned long n) 
+{
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  int t=300;
+
+  Serial.print(" Button held down for: ");
+  Serial.print(n);
+  Serial.print("ms  - ");
+  Serial.print("Service 2 started.");
+  for(int i=0; i<3; i++)
+  {
+    digitalWrite(LED_G, HIGH); 
+    digitalWrite(LED_R, HIGH); 
+    delay(t); Serial.print("..");
+    digitalWrite(LED_G, LOW); 
+    digitalWrite(LED_R, LOW); 
+    delay(t); Serial.print("..");
+  }  
+  Serial.println(" service 2 ended.");
+}
+
+/*
+// void MonitorChange1();
+int cMonitor1Change=0;
+PBmonitor button1(PB2, FlashLeds, MonitorChange1, LOW, 30);
+void MonitorChange1() // global user defined void f() to be used for ISR from inside the class
+{
+  cMonitor1Change++; // can contain other code...
+  button1.Change(); // must contain this call
+}
+
+// void MonitorChange2();
+int cMonitor2Change=0;
+PBmonitor button2(PB1, BlinkLeds, MonitorChange2, HIGH, 100); // puldown resistor must be used
+void MonitorChange2() // global user defined void f() to be used for ISR from inside the class
+{
+  cMonitor2Change++;
+  button2.Change();
+}
+*/
+
+// Define the push buttons to be monitored
+PUSH_BUTTON_L(button1, PB2, FlashLeds);
+//SET_PB(button1, PB2, FlashLeds, MonitorChange1, LOW, 20);
+//PUSH_BUTTON_H(button2, PB1, BlinkLeds);
+SET_PB(button2, PB1, BlinkLeds, MonitorChange2, HIGH, 100);
+
+void setup() {
+  // put your setup code here, to run once:
+
+//  FlashLeds(3);
+///////////////////
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("Push button from interrupt example ...");
+  Serial.println("Buttons:");
+  Serial.print("Button #1 @pin2");
+  Serial.print(button1.Type()?"(active HIGH)":"(active  LOW)");
+  Serial.println(button1.IsMonitoring()?"  monitored":"   idle");
+  Serial.print("Button #2 @pin3");
+  Serial.print(button2.Type()?"(active HIGH)":"(active  LOW)");
+  Serial.println(button2.IsMonitoring()?"  monitored":"   idle");
+
+  Serial.println("Start monitoring...");
+
+// Mandatory - initiate monitoring
+  button1.StartMonitoring();
+  button2.StartMonitoring();
+
+  Serial.print("Button #1 @pin2");
+  Serial.print(button1.Type()?"(active HIGH)":"(active  LOW)");
+  Serial.println(button1.IsMonitoring()?"  monitored":"   idle");
+  Serial.print("Button #2 @pin3");
+  Serial.print(button2.Type()?"(active HIGH)":"(active  LOW)");
+  Serial.println(button2.IsMonitoring()?"  monitored":"   idle");
+
+  Serial.println("Push button ready ...");
+
+} // end setup
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Nothing really happens here - EVERYTHING is interrupt driven
+  // you can put any other code to monitor other sensoer / perform other tasks
+
+  // Just monitoring the current state od the pins 2 and 3 (push button 1 & 2 are connected to)
+  // the functions will fre up on button presses and will print on the serial that will appear COM monitor
+  Serial.print("digitalRead(2)=");
+  Serial.print(digitalRead(2));
+  Serial.print("  ");
+  Serial.print("digitalRead(3)=");
+  Serial.print(digitalRead(3));
+  Serial.println();
+  
+  delay(1000);
+}
+
---
+++ id_PBmonitor.h
@@ -0,0 +1,121 @@
+/*
+  PBMonitor v 1.0 - Interrupt driven push button monitor with simple debouncing
+  Triggers a call to a user defined call back function on release of the button
+  Reports back the duration in milliseconds the button has been held down before releasing 
+  so that long and short presses can be determined inside the user function invoked by the button
+
+  Active on low and active on high push buttons are supported. If active on low (when pushed connects the pin to GND) 
+  internal pullup resistor is configured, so there is no need for installing a pullup resistor in the circuitry
+  BUT if active on high pushbutton is used a pulldown resistor MUST be installed in the circuitry
+
+  Parameters that are specified in definition of the PBMonitor object:
+  - The pin to which the button is connected (must be a pin that can generate hardware interrupt requests - see table below)
+  - The type of the button - active high or low (will behave strange if not defined according to the way it is connected)
+  - The function to be called when the button is pressed (released to be exact)
+  - The function to serve as a ISR - must be void f() and must contain a call to the Change() member function of that 
+    PBMonitor class for the object (can be generated automatically if the macro definition for declaring a PBmonitor is used)
+  - The minimum time pressed to be registered - to avoid bouncing (a press shorter than the specified time will be ignored) 
+  
+ created 22.04.2016
+ by Dejan Gjorgjevikj
+ 
+ - Hardware interrupts
+  Board     int.0   int.1   int.2   int.3   int.4   int.5
+ Uno, Nano  2   3
+ Mega2560   2   3   21    20    19    18
+ Leonardo   3   2   0   1
+ Due            (any pin, more info http://arduino.cc/en/Reference/AttachInterrupt)
+
+ This example use 2 interrupt driven push buttons at the same time
+ */ 
+
+// Macros to automate Push button object instatiation with interrupt service routine global function definition 
+// Interrupts needs a void function (not part of the class) to act as interrupt service routine that must be defined outside the class 
+// as a wrapper to member function to be called on button change state (from the interrupt)
+#define SET_PB(PBUTON, PIN_BUT, FP_CALLB, FP_ISR, TYPE, T_IGNORE) PBmonitor PBUTON(PIN_BUT, FP_CALLB, PBUTON##_ISR, TYPE, T_IGNORE); \
+void PBUTON##_ISR() { PBUTON.Change(); }
+#define PUSH_BUTTON_L(PBUTON, PIN_BUT, FP_CALLB) PBmonitor PBUTON(PIN_BUT, FP_CALLB, PBUTON##_ISR, LOW); \
+void PBUTON##_ISR() { PBUTON.Change(); }
+#define PUSH_BUTTON_H(PBUTON, PIN_BUT, FP_CALLB) PBmonitor PBUTON(PIN_BUT, FP_CALLB, PBUTON##_ISR, HIGH); \
+void PBUTON##_ISR() { PBUTON.Change(); }
+
+typedef void (*ISR)(); // pointer to void function (to act as interrupt service routine)
+typedef void (*PBcallback) (unsigned long); // pointer to void function taking one int (should be unsigned long) 
+// to be called from the push button monitor object when the button wil be released passing the number od miliseconds the button was held down
+
+class PBmonitor // the class represntin a push button to be monitored using interrupts
+{
+  public:
+    PBmonitor(int p, PBcallback f, ISR isrv, bool act = HIGH, unsigned long t_i = 20) : 
+      pin(p), isr(isrv), callback(f), active(act), t_ignore(t_i), e_millis(0), inCallback(false), pstate(act?LOW:HIGH), monitoring(false) 
+      { /* assert(digitalPinToInterrupt(p) != NOT_AN_INTERRUPT); */ }
+    ~PBmonitor() { StopMonitoring(); }
+    void StartMonitoring() 
+    { 
+      digitalWrite(pin, active==LOW ? HIGH : LOW); 
+      pinMode(pin, active==LOW ? INPUT_PULLUP : INPUT); // internal pull-up resistor or external pulldown
+      pstate = digitalRead(pin);
+      monitoring=true;
+      attachInterrupt(digitalPinToInterrupt(pin), isr, CHANGE); // set interrupt on change
+    }
+    void StopMonitoring() 
+    { 
+      detachInterrupt(digitalPinToInterrupt(pin)); 
+      monitoring=false; 
+    }
+    bool IsMonitoring() const { return monitoring; } // is monitored = ISR installed
+    bool Type() const { return active; } // active high or low
+    
+    void Change() 
+    { 
+      bool push_registered=false;
+      uint8_t oldSREG = SREG; // Save the status
+      interrupts();
+      unsigned long now=millis();
+      SREG = oldSREG;
+      bool state = digitalRead(pin);
+      if(pstate == HIGH && state == LOW)
+      {
+        if(active == HIGH)
+          push_registered=true;
+        else
+          e_millis = now; // just store the time when pushed down
+      }
+      else if(pstate == LOW && state == HIGH)
+      { // released - take action now
+        if(active == HIGH)
+          e_millis = now; // just store the time when pushed down
+        else
+          push_registered=true;
+      }
+      pstate=state;
+      if(push_registered && now-e_millis > t_ignore) // was pressed long enought
+      {
+        if(!inCallback) 
+        {
+          //detachInterrupt(digitalPinToInterrupt(pin)); // disable monitoring the buttin while in service
+          //monitoring=false;
+          inCallback=true;
+          interrupts();
+          callback(now-e_millis);
+          noInterrupts();
+          inCallback=false;
+          //monitoring=true;
+          //attachInterrupt(digitalPinToInterrupt(pin), isr, CHANGE); // set interrupt on change again
+        }
+      }
+    }
+
+  private:
+    bool monitoring;
+    int pin; // pin at which change of level is monitored
+    PBcallback callback; // pointer to function to be called when button is pressed
+    ISR isr; // pointer to void f() function to serve as interrupt service routine - must be defined on a global scope
+    bool active; // type of switch active on HIGH or LOW, when set to be active on HIGH, pulldown resistor must be connected
+    bool inCallback; // true while executing the callback function, will not re-enter the function from the same interrupt (isr disabled)
+    bool pstate; // previous state of the pin (checked in the ISR)
+    unsigned long e_millis; // elapsed millis since the last call to ISR
+    unsigned long t_ignore; // time to be ignorred - changes that appear @ t < t_ignore will be ignored
+};
+
+
